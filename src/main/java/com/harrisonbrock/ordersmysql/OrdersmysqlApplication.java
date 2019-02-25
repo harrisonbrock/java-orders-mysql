72cb92e7741334a72e34bebf1fc51d538c4d0a33
Harrison Brock <harrison@harrisonbrock.com>
2019-02-25 15:06:09 -0500

    create new project

---
+++ src/main/java/com/harrisonbrock/ordersmysql/OrdersmysqlApplication.java
@@ -0,0 +1,13 @@
+package com.harrisonbrock.ordersmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersmysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersmysqlApplication.class, args);
+    }
+
+}
